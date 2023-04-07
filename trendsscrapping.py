7e933569e714be2f64ddededac1047a0b621a1eb
Shashank <shashank21005@gmail.com>
2023-04-08 02:08:51 +0530

    initial commit

---
+++ trendsscrapping.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://trends.google.com/trends/trendingsearches/realtime?geo=IN&hl=en-GB&category=all'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+trending_searches = soup.find_all('div', {'class': 'title'})
+for index, search in enumerate(trending_searches[:20]):
+    print(f"{index+1}. {search.text}")
+print(soup.prettify())
